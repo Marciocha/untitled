22781d4daa0da384c5d9f2d02dd541e4eb0e8d8a
Marciocha <taleja555@interia.pl>
2017-03-04 12:52:25 +0100

    add final files

---
+++ src/com/sdajava/if_statement1/Main.java
@@ -0,0 +1,24 @@
+package com.sdajava.if_statement1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a, b, c;
+
+        a = 3;
+        b = 4;
+        c = 5;
+
+
+        if((a*a)+(b*b)==(c*c));
+        {
+            System.out.println("Tworzą trójkąt prostokątny");
+        }
+
+        }
+        }
+
+
+    }
+
